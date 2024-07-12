49ae4bed0cb17c3db6b6e71ed983cd014ac8f3f4
Revanth Reddy <106379842+RevanthReddyBogala@users.noreply.github.com>
2024-07-12 20:29:36 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "Docker.pem" 
+  key_name      = "aws-key" 
   tags = {
     Name = var.instance_name
   }
